d9b6ea1e704855372bce3e1bb795f1c7a45b797c
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-02-12 20:14:47 -0800

    added dropzone

---
+++ src/AcceptMaxFiles.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import classnames from "classnames";
+// import { useDropzone } from "react-dropzone";
+import { DropzoneOptions, useDropzone } from "react-dropzone";
+import "./styles/animate-dropzone.scss";
+import "./styles/dropzone.scss";
+
+export function AcceptMaxFiles(_props: any) {
+  const {
+    acceptedFiles,
+    isDragActive,
+    fileRejections,
+    getRootProps,
+    getInputProps,
+    open,
+  } = useDropzone({
+    maxFiles: 1,
+  });
+
+  const dropzoneClass = "";
+  const multiple = false;
+
+  const acceptedFileItems = acceptedFiles.map((file: File) => (
+    <li key={file.webkitRelativePath}>
+      {file.name} - {file.size} bytes
+    </li>
+  ));
+
+  const fileRejectionItems = fileRejections.map(({ file, errors }) => {
+    return (
+      <li key={file.webkitRelativePath}>
+        {file.name} - {file.size} bytes
+        <ul>
+          {errors.map((e) => (
+            <li key={e.code}>{e.message}</li>
+          ))}
+        </ul>
+      </li>
+    );
+  });
+  const className = "custom-wrapper";
+  const isInvalid = false;
+  const clickAnywhereForUpload = true;
+  return (
+    <section className="container">
+      <div
+        className={classnames([
+          `dropzone-container`,
+          className,
+          isDragActive ? "drag-active" : "",
+          isInvalid ? "invalid" : "",
+        ])}
+        onClick={clickAnywhereForUpload ? open : () => {}}
+        {...getRootProps()}
+      >
+        <div className="topbottom" />
+        <div className="leftright" />
+        <div className={`dropzone ${dropzoneClass}`}>
+          <div className="instruction">
+            <p>Drag and drop your files here.</p>
+            {
+              <div>
+                <button className="uploadBtn" onClick={open}>
+                  Click Here
+                </button>
+              </div>
+            }
+          </div>
+        </div>
+
+        <input {...getInputProps()} multiple={multiple} />
+      </div>
+      {/* <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+        <em>(1 files are the maximum number of files you can drop here)</em>
+      </div> */}
+      <aside>
+        <h4>Accepted files</h4>
+        <ul>{acceptedFileItems}</ul>
+        <h4>Rejected files</h4>
+        <ul>{fileRejectionItems}</ul>
+      </aside>
+    </section>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import { AppBar, Toolbar } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import { AcceptMaxFiles } from "./AcceptMaxFiles";
 function App() {
+  const [value, setValue] = useState<File | null>(null);
+
+  const handleChange = (newValue: File | null) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="sticky">
+        <Toolbar>text to pdf to image to text</Toolbar>
+      </AppBar>
+      <Container>
+        <Box sx={{ my: 4 }}>
+          <TextField
+            id="outlined-multiline-static"
+            label="Enter any text"
+            multiline
+            fullWidth
+            rows={10}
+            placeholder="Enter any text"
+          />
+          <AcceptMaxFiles />{" "}
+        </Box>
+      </Container>
     </div>
   );
 }
